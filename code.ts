47fd9fc2f1665674011c84e64dfd4a4974d9d51b
Lennard Dorst <lennardsolana@gmail.com>
2022-11-29 13:17:48 +0100

    Creating rectangle of users viewport size

--- code.ts
+++ code.ts
@@ -1,5 +1,11 @@
 figma.showUI(__html__)
 
 figma.ui.onmessage = pluginMessage => {
-  console.log(pluginMessage.action)
+  const rect = figma.createRectangle()
+  rect.x = figma.viewport.bounds.x
+  rect.y = figma.viewport.bounds.y
+
+  rect.resize(100 / figma.viewport.zoom, 100 / figma.viewport.zoom)
+  
+  rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }]
 }
