59aac65820e7fdb0666b037e3b007c0c8c7eee89
Lennard Dorst <lennardsolana@gmail.com>
2022-11-29 18:54:17 +0100

    Adding frame & title

--- code.ts
+++ code.ts
@@ -1,11 +1,28 @@
 figma.showUI(__html__)
 
-figma.ui.onmessage = pluginMessage => {
-  const rect = figma.createRectangle()
-  rect.x = figma.viewport.bounds.x
-  rect.y = figma.viewport.bounds.y
+figma.ui.onmessage = async (pluginmessage) => {
+  const nodes: SceneNode[] = [];
 
-  rect.resize(100 / figma.viewport.zoom, 100 / figma.viewport.zoom)
-  
-  rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }]
+  const frame = figma.createFrame()
+  frame.resizeWithoutConstraints(figma.viewport.bounds.width / 2.5, figma.viewport.bounds.height / 2.5)
+
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" })
+
+  let title = figma.createText();
+  title.x = frame.x + (frame.height / 2)
+  title.y = frame.y + (frame.width / 10)
+
+  title.characters = "Title";
+  title.textAutoResize = "WIDTH_AND_HEIGHT"
+  title.fontSize = figma.viewport.bounds.width / 30
+
+  frame.appendChild(title);
+
+  nodes.push(frame);
+
+  figma.viewport.scrollAndZoomIntoView(nodes);
+
+
+  figma.currentPage.selection = nodes;
+
 }
