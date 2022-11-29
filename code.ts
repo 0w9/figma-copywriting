e1708cb02e2c368d5503650dfe713d3655b43054
Lennard Dorst <lennardsolana@gmail.com>
2022-11-29 22:22:12 +0100

    Fixing the relative sizing for demo...

--- code.ts
+++ code.ts
@@ -11,14 +11,30 @@ figma.ui.onmessage = async (pluginmessage) => {
   await figma.loadFontAsync({ family: "Inter", style: "Regular" })
 
   let title = figma.createText();
-  title.x = frame.x + (frame.height / 2)
-  title.y = frame.y + (frame.width / 10)
 
   title.characters = "Title";
   title.textAutoResize = "WIDTH_AND_HEIGHT"
-  title.fontSize = figma.viewport.bounds.width / 30
+  title.textAlignHorizontal = "CENTER"
+
+  title.fontSize = figma.viewport.bounds.width / 50
+  title.resize(frame.width, title.height)
+
+  title.x = frame.x
+  title.y = frame.y + frame.height / 7.50
+
+  let text = figma.createText();
+  text.resize(0.75 * frame.width, text.height)
+
+  text.x = 77 - 22
+  text.y = frame.y + frame.height / 3
+
+  text.characters = "Your content is ready!";
+  text.textAutoResize = "WIDTH_AND_HEIGHT"
+  text.textAlignHorizontal = "CENTER"
+  text.fontSize = figma.viewport.bounds.width / 75
 
   frame.appendChild(title);
+  frame.appendChild(text);
 
   nodes.push(frame);
 
